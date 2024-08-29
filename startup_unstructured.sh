5380ced6ff3691f4d37fbd9c381fefae4890992d
hyogrin <hyogrin@gmail.com>
2024-08-29 10:01:00 +0000

    shell script update

--- startup_unstructured.sh
+++ startup_unstructured.sh
@@ -2,19 +2,28 @@
 
 set -e
 
-sudo -u azureuser -i <<'EOF'
-
-ENVIRONMENT=azureml_py310_sdkv2
-conda activate "$ENVIRONMENT"
-
+# Run sudo commands as root
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:alex-p/tesseract-ocr5 -y
 sudo apt-get update -y
 sudo apt-get install poppler-utils tesseract-ocr tesseract-ocr-eng tesseract-ocr-kor -y
 sudo apt-get install libgl1-mesa-glx libglib2.0-0 -y
-sudo apt install libreoffice -y
-pip install -U "unstructured[all-docs]"
+sudo apt-get install libreoffice -y
+
+# Switch to azureuser and run the rest of the commands
+sudo -u azureuser bash <<'EOF'
+source ~/.bashrc
+
+# Find and source conda initialization script
+
+CONDA_DIR=/anaconda
+source "$CONDA_DIR/etc/profile.d/conda.sh"
+
+ENVIRONMENT=azureml_py310_sdkv2
+conda activate "$ENVIRONMENT"
+
+pip install --upgrade pip
+pip install -U unstructured[all-docs]
 pip install -r requirements.txt
 
-conda deactivate
 EOF
