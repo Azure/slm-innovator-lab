c4886f94988b33bb26d41b7de134bf9ecc3678af
hyogrin <hyogrin@gmail.com>
2024-12-03 14:25:48 +0000

    update for inference api specs

--- 3_llmops-aistudio/3_2_prototyping/chat-serverless/phi35_chatcompletion.py
+++ 3_llmops-aistudio/3_2_prototyping/chat-serverless/phi35_chatcompletion.py
@@ -2,9 +2,13 @@ import urllib
 import json
 from promptflow import tool
 from promptflow.connections import CustomConnection
-import requests
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import SystemMessage
+from azure.ai.inference.models import UserMessage
+from azure.core.credentials import AzureKeyCredential
+from urllib.parse import urlparse, urlunparse
 
-def chat(question: str, context:str, connection: CustomConnection) -> str:
+def chat(deployment_name:str, question: str, context:str, connection: CustomConnection) -> str:
     
     # Request data goes here
     # The example below assumes JSON formatting which may be updated
@@ -12,42 +16,43 @@ def chat(question: str, context:str, connection: CustomConnection) -> str:
     # More information can be found here:
     # https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
     endpoint_url = connection.endpoint
+    parsed_url = urlparse(endpoint_url)
+    new_url = urlunparse((parsed_url.scheme, parsed_url.netloc, '/models', '', '', ''))
+
     api_key = connection.key
     
 
     if not api_key:
         raise Exception("A key should be provided to invoke the endpoint")
 
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {api_key}"
-    }
-
-    data = {
-        "messages": 
-            [
-                {"role": "user", "content": "You are an AI assistant who helps people find information. As the assistant, you answer questions not long, simple, short."},
-                {"role": "user", "content": "Use the following context to provide a more personalized response to the customer:"},
-                {"role": "user", "content": context},
-                {"role": "user", "content": question},
-                
-            ],
-        "parameters": {
-                "temperature": 0.2,
-                "max_new_tokens": 256,
-                "do_sample": True,
-                "return_full_text": False
-        }
-    }
-    
     try:
-        response = requests.post(endpoint_url, json=data, headers=headers)
-        response.raise_for_status()
-        result = response.json()
-        result = result["choices"][0]['message']['content']
+        client = ChatCompletionsClient(
+            endpoint=new_url, # you will run into a 500 error if you use this endpoint
+            credential=AzureKeyCredential(api_key)
+        )
+        
+
+        response = client.complete(
+            messages=[
+                SystemMessage(content="""
+                You are an AI assistant who helps people find information. As the assistant, you answer questions not long, simple, short.
+                Use the following context to provide a more personalized response to the customer. 
+                """),
+
+            
+                UserMessage(content=f"""
+                Context: {context}
+                Question: {question}
+                """),
+            ],
+            # Simply change the model name for the appropiate model "Phi-3.5-mini-instruct" or "Phi-3.5-vision-instruct"
+            model=deployment_name, 
+            temperature=0.8,
+            max_tokens=256
+        )
+
+        result = response.choices[0].message.content
         return result
-    except requests.exceptions.RequestException as e:
-        print(e)
     except urllib.error.HTTPError as error:
         print("The request failed with status code: " + str(error.code))
 
@@ -56,8 +61,8 @@ def chat(question: str, context:str, connection: CustomConnection) -> str:
         print(error.read().decode("utf8", 'ignore'))
 
 @tool
-def my_python_tool(question: str, context:str, connection: CustomConnection) -> str:
+def my_python_tool(deployment_name:str, question: str, context:str, connection: CustomConnection) -> str:
     """
     Tool function to process input data and query the Phi-3 model.
     """
-    return chat(question, context, connection)
+    return chat(deployment_name, question, context, connection)
